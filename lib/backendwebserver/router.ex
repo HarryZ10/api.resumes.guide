cfdff3a6f3e72e4eb106d286e22736eb776b55ca
Harry <harryzhu45@gmail.com>
2024-05-23 16:42:04 -0700

    Add cors

--- lib/backendwebserver/router.ex
+++ lib/backendwebserver/router.ex
@@ -2,16 +2,12 @@ defmodule Backendwebserver.Router do
   require Logger
   use Plug.Router
 
+  plug Corsica, origins: "*"
   plug :match
   plug :dispatch
 
-  get "/" do
-    # GET request to locahost:8000
-    send_resp(conn, 200, Jason.encode!(%{
-      message: "Resume Syntax Generator API",
-      status: 200
-    }))
-  end
+  options "/generate",
+    do: Corsica.send_preflight_resp(conn, origins: "*")
 
   get "/generate" do
     github_raw_url = "https://raw.githubusercontent.com/HarryZ10/resumes.guide/main/static/resume.g"
